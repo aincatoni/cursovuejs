d9ae008c34e88c8362103c277beb3060a33510f1
Ain Cortés Catoni <acortes@pcfactory.cl>
2023-07-17 19:08:40 -0400

    termina la sección 5 y la app de indecisión

---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h1>{{ customTitle }}</h1>
+    <p> {{counter}} <sup>2</sup> = {{ squareCounter}}</p>
+
+    <div>
+      <button @:click="increase">+1</button>
+      <button @:click="decrease">-1</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 100,
+      validator(value) {
+        return value > -1
+      } 
+      // required: true
+
+    },
+  },
+  // name: 'Counter'
+  data(){
+    return {
+      counter: this.start,
+    }
+  },
+  methods:{
+    getSquareValue() {
+      return this.counter
+    },
+    increase () {
+     this.counter++;
+    },
+    decrease () {
+      
+      this.counter--;
+    },
+  },
+  computed: {
+    squareCounter () {
+      return this.counter * this.counter
+    },
+    customTitle () {
+      return this.title ? this.title : "Counter"
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  background-color: #64b687;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3 ease-in-out;
+
+}
+ button:hover {
+  background-color:  #5aa67b;
+  transition: 0.3 ease-in-out;
+ }
+
+</style>
