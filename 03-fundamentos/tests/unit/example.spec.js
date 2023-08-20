91990d6d01bd6e1d1d78d84d9c83b84b8f387888
Ain Cortés Catoni <acortes@pcfactory.cl>
2023-08-20 17:12:31 -0400

    actualiza repo

--- 03-fundamentos/tests/unit/example.spec.js
+++ 03-fundamentos/tests/unit/example.spec.js
@@ -1,12 +1,25 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+
+
+describe('Example Component', ()=> {
+  test( 'Debe de ser mayor a 10', () =>{
+
+    // Arreglar
+    let value = 9;
+
+    // Estimulo
+    value = value + 2
+
+    // Observar el resultado
+    if (value > 10) {
+      //Todo bien
+    } else {
+      throw `${value} no es mayor a 10`
+    }
+
   })
+
 })
+
+
+
